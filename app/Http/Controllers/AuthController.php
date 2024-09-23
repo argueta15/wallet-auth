6284767e3426ce81248a3e3274d0715be92c3a19
Gerardo Argueta <gerardo@sellia.com.mx>
2024-09-23 16:56:30 -0600

    first commit

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use App\Models\User;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        // Validar los datos de entrada
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 422);
+        }
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        return response()->json([
+            'message' => 'Usuario registrado exitosamente',
+            'user' => $user,
+        ], 201);
+    }
+
+    public function validateToken(Request $request)
+    {
+        return response()->json(['user' => $request->user()]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+
+Route::post('register', 'App\Http\Controllers\AuthController@register');
+Route::get('validate-token', 'App\Http\Controllers\AuthController@validateToken')->middleware('auth:api');
